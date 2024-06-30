e2da8e926ce2e1a1ba379460b5c031fc954520b3
MihajloCuic <158320399+MihajloCuic@users.noreply.github.com>
2024-06-30 18:12:53 +0200

    Dodat model glasanja

---
+++ MuzickiKatalog/Model/Glas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MuzickiKatalog.Model
+{
+    public class Glas
+    {
+        private Osoba glasac;
+        private ElementSistema izglasaniElement;
+
+        public Osoba Glasac { get; set; }
+        public ElementSistema IzglasaniElement { get; set; }
+    }
+}
---
+++ MuzickiKatalog/Model/Glasanje.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MuzickiKatalog.Model
+{
+    public class Glasanje
+    {
+        private int id;
+        private DateTime datumPocetka;
+        private DateTime datumZavrsetka;
+        private string imeTakmicenja;
+        private List<ElementSistema> kandidati;
+        private List<Glas> glasovi;
+
+        public int Id { get; set; }
+        public DateTime DatumPocetka { get; set; }
+        public DateTime DatumZavrsetka { get; set; }
+        public string ImeTakmicenja { get; set; }
+        public List<ElementSistema> Kandidati { get; set; }
+        public List<Glas> Glasovi { get; set; }
+    }
+}
