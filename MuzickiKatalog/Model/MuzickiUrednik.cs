79fabcef335b461ecc0b19036ec225a24fa38954
MihajloCuic <158320399+MihajloCuic@users.noreply.github.com>
2024-07-01 19:59:59 +0200

    Izmena MuzickiUrednik.cs dodate CRUD funkcionalnosti

--- MuzickiKatalog/Model/MuzickiUrednik.cs
+++ MuzickiKatalog/Model/MuzickiUrednik.cs
@@ -1,18 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace MuzickiKatalog.Model
 {
     class MuzickiUrednik : Osoba
     {
+        private static readonly string fajl = Path.Combine("..", "..", "..", "Data", "MuzickiUrednici.json");
         private List<Zanr> sviZanrovi;
-
         public List<Zanr> SviZanrovi { get; set; }
         //base konstruktor
         public MuzickiUrednik() 
+            :base()
         {
             SviZanrovi = new List<Zanr>();
         }
@@ -28,5 +32,94 @@ namespace MuzickiKatalog.Model
         {
             SviZanrovi = _sviZanrovi;
         }
+        //citanje muzickih urednika iz fajla
+        public static Dictionary<string, MuzickiUrednik> UcitajUrednike()
+        {
+            Dictionary<string, MuzickiUrednik> sviUrednici = new Dictionary<string, MuzickiUrednik>();
+            try
+            {
+                string data = File.ReadAllText(fajl);
+                sviUrednici = JsonConvert.DeserializeObject<Dictionary<string, MuzickiUrednik>>(data);
+            }
+            catch (IOException ex)
+            {
+                throw new Exception("Greska u citanju fajla!");
+            }
+            return sviUrednici;
+        }
+        //pisanje muzickih urednika u fajl
+        public static void UpisiUrednike(Dictionary<string, MuzickiUrednik> sviUrednici) 
+        {
+            try
+            {
+                string data = JsonConvert.SerializeObject(sviUrednici, Formatting.Indented);
+                File.WriteAllText(fajl, data);
+            }
+            catch (IOException ex)
+            {
+                throw new Exception("Greska u pisanju u fajl!");
+            }
+        }
+        //Dodaj muzickog urednika
+        public void Dodaj() 
+        { 
+            Dictionary<string, MuzickiUrednik> sviUrednici = UcitajUrednike();
+            if (sviUrednici.ContainsKey(Id))
+            {
+                throw new Exception("Urednik vec postoji!");
+            }
+
+            sviUrednici[Id] = this;
+            UpisiUrednike(sviUrednici);
+        }
+        //Izmeni muzickog urednika
+        public void Izmeni(string ime, string prezime, string telefon,
+            List<Recenzija> sveRecenzije, List<Zanr> sviZanrovi) 
+        {
+            Ime = ime;
+            Prezime = prezime;
+            Telefon = telefon;
+            SveRecenzije = sveRecenzije;
+            SviZanrovi = sviZanrovi;
+
+            Dictionary<string, MuzickiUrednik> sviUrednici = UcitajUrednike();
+            if (!sviUrednici.ContainsKey(Id))
+            {
+                throw new Exception("Ne postoji trazeni urednik!");
+            }
+            sviUrednici[Id] = this;
+            UpisiUrednike(sviUrednici);
+        }
+        //Obrisi muzickog urednika
+        public void Obrisi() 
+        {
+            Dictionary<string, MuzickiUrednik> sviUrednici = UcitajUrednike();
+            if (!sviUrednici.ContainsKey(Id))
+            {
+                throw new Exception("Ne postoji trazeni urednik!");
+            }
+            sviUrednici.Remove(Id);
+            UpisiUrednike(sviUrednici);
+        }
+        //Prikaz dodeljenih recenzija
+        public List<Recenzija> PrikaziDodeljeneRecenzije() 
+        {
+            List<Recenzija> dodeljeneRecenzije = new List<Recenzija>();
+            //TODO dodati logiku za prikaz dodeljenih recenzija
+            return dodeljeneRecenzije;
+        }
+        //Dodavanje novog elementa
+        //public ElementSistema DodavanjeElementaSistema() {}
+        //Dodavanje nove recenzije
+        public void OstaviRecenziju(Recenzija novaRecenzija) 
+        {
+            Dictionary<string, MuzickiUrednik> sviUrednici = UcitajUrednike();
+            sviUrednici[Id].SveRecenzije.Add(novaRecenzija);
+            UpisiUrednike(sviUrednici);
+        }
+        //brisanje neprikladne korisnicke recenzije
+        public void ModerirajKorisnickuRecenziju(Korisnik korisnik) { }
+        //Glasaj
+        public void Glasaj() { }
     }
 }
