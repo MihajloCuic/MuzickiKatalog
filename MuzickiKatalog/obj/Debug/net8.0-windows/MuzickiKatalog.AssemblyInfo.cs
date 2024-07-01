a40a887a3a0a4c6e1587bb2a419cbd552ebe75ed
MihajloCuic <158320399+MihajloCuic@users.noreply.github.com>
2024-07-02 00:38:28 +0200

    Dodati View, Data i Controller folderi

--- MuzickiKatalog/obj/Debug/net8.0-windows/MuzickiKatalog.AssemblyInfo.cs
+++ MuzickiKatalog/obj/Debug/net8.0-windows/MuzickiKatalog.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MuzickiKatalog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c432c346584263e2c46e97869796febf947126c")]
 [assembly: System.Reflection.AssemblyProductAttribute("MuzickiKatalog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MuzickiKatalog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
