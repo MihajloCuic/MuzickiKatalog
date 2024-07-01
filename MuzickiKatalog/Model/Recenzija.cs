7cea4818c35960cbc80d6b2399dd766bd1a732ec
MihajloCuic <158320399+MihajloCuic@users.noreply.github.com>
2024-07-02 00:33:32 +0200

    Izmena klase Recenzija.cs dodate CRUD operacije

--- MuzickiKatalog/Model/Recenzija.cs
+++ MuzickiKatalog/Model/Recenzija.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.IO.Packaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,13 +11,92 @@ namespace MuzickiKatalog.Model
 {
     public class Recenzija
     {
+        private static readonly string fajl = Path.Combine("..", "..", "..", "Data", "Recenzije.json");
         private int id;
         private string opis;
-        private int ocena;
+        private double ocena;
+        private Osoba recezent;
+        private ElementSistema recenziraniElement;
 
         public int Id { get; set; }
         public string Opis { get; set; }
         public int Ocena { get; set; }
+        public Osoba Recezent { get; set; }
+        public ElementSistema RecenziraniElement { get; set; }
+        //base konstruktor
+        public Recenzija() { }
+        //parametarski konstruktor
+        public Recenzija(int _id, string _opis, int _ocena, Osoba _recezent, ElementSistema _recenziraniElement)
+        {
+            Id = _id;
+            Opis = _opis;
+            Ocena = _ocena;
+            Recezent = _recezent;
+            RecenziraniElement = _recenziraniElement;
+        }
+        //Citanje iz fajla
+        public static Dictionary<int, Recenzija> UcitajRecenzije() 
+        {
+            Dictionary<int, Recenzija> sveRecenzije = new Dictionary<int, Recenzija>();
+            try
+            {
+                string data = File.ReadAllText(fajl);
+                sveRecenzije = JsonConvert.DeserializeObject<Dictionary<int, Recenzija>>(data);
+            }
+            catch (IOException ex)
+            {
+                throw new Exception("Greska pri citanju fajla!");
+            }
+            return sveRecenzije;
+        }
+        //Pisanje u fajl
+        public static void UpisiRecenzije(Dictionary<int, Recenzija> sveRecenzije)
+        {
+            try
+            {
+                string data = JsonConvert.SerializeObject(sveRecenzije, Formatting.Indented);
+                File.WriteAllText(fajl, data);
+            }
+            catch (IOException ex)
+            {
+                throw new Exception("Greska pri upisu u fajl!");
+            }
+        }
+        //Dodaj recenziju
+        public void Dodaj() 
+        {
+            Dictionary<int, Recenzija> sveRecenzije = UcitajRecenzije();
+            if (sveRecenzije.ContainsKey(Id))
+            {
+                throw new Exception("Recenzija vec postoji!");
+            }
+            sveRecenzije[Id] = this;
+            UpisiRecenzije(sveRecenzije);
+        }
+        //Izmeni recenziju
+        public void Izmeni(string opis, int ocena) 
+        {
+            Opis = opis;
+            Ocena = ocena;
 
+            Dictionary<int, Recenzija> sveRecenzije = UcitajRecenzije();
+            if (!sveRecenzije.ContainsKey(Id))
+            {
+                throw new Exception("Ne postoji trazena recenzija!");
+            }
+            sveRecenzije[Id] = this;
+            UpisiRecenzije(sveRecenzije);
+        }
+        //Obrisi recenziju
+        public void Obrisi() 
+        {
+            Dictionary<int, Recenzija> sveRecenzije = UcitajRecenzije();
+            if (!sveRecenzije.ContainsKey(Id))
+            {
+                throw new Exception("Ne postoji trazena recenzija!");
+            }
+            sveRecenzije.Remove(Id);
+            UpisiRecenzije(sveRecenzije);
+        }
     }
 }
