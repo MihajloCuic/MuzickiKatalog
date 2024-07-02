880f38a3f3dca399e3ce704d29d763c118d6accf
MihajloCuic <158320399+MihajloCuic@users.noreply.github.com>
2024-07-02 17:30:06 +0200

    izmena gitignore fajla

--- MuzickiKatalog/obj/Debug/net8.0-windows/MuzickiKatalog.AssemblyInfo.cs
+++ MuzickiKatalog/obj/Debug/net8.0-windows/MuzickiKatalog.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MuzickiKatalog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f6d740eaccb8712af972e75f6897c201fca93ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("MuzickiKatalog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MuzickiKatalog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
