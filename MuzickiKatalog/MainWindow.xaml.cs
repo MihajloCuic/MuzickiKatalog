841faa9c9a4b1c8661b3404e3b6880b83b2d00e3
milosm1707 <137085530+milosm1707@users.noreply.github.com>
2024-07-02 20:47:48 +0200

    UPDATE ElementiSistema
    Dodate CRUD operacije za sve elemente sistema

--- MuzickiKatalog/MainWindow.xaml.cs
+++ MuzickiKatalog/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using MuzickiKatalog.Model;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -18,6 +19,7 @@ namespace MuzickiKatalog
     {
         public MainWindow()
         {
+            //MFunctionsES.test1();
             InitializeComponent();
         }
     }
--- MuzickiKatalog/Model/Izvodjac.cs
+++ MuzickiKatalog/Model/Izvodjac.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace MuzickiKatalog.Model
 {
     public class Izvodjac : ElementSistema
     {
+        private static readonly string file = Path.Combine("..", "..", "..", "Data", "Izvodjaci.json");
         private List<MuzickaNumera> numere;
         private MuzickaGrupa grupa;
 
@@ -34,6 +38,85 @@ namespace MuzickiKatalog.Model
         { 
             Grupa = _muzickaGrupa;
             Numere = _numere;
+        }
+        //citanje izvodjaca iz fajla
+        public static Dictionary<int, Izvodjac> UcitajIzvodjace()
+        {
+            Dictionary<int, Izvodjac> sviIzvodjaci = new Dictionary<int, Izvodjac>();
+            try
+            {
+                string data = File.ReadAllText(file);
+                sviIzvodjaci = JsonConvert.DeserializeObject<Dictionary<int, Izvodjac>>(data);
+            }
+            catch(IOException e)
+            {
+                throw new Exception("Greska pri citanju iz fajla");
+            }
+            return sviIzvodjaci; 
+        }
+        //pisanje izvodjaca u fajl
+        public static void UpisiIzvodjace(Dictionary<int,Izvodjac> _sviIzvodjaci)
+        {
+            try
+            {
+                JsonSerializerSettings settings = new JsonSerializerSettings
+                {
+                    Formatting = Formatting.Indented,
+                    ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+                };
+                string data = JsonConvert.SerializeObject(_sviIzvodjaci, settings);
+                File.WriteAllText(file, data);
+            }
+            catch(FileNotFoundException e)
+            {
+                throw new Exception("Greska pri upisivanju u fajl");
+            }
+        }
+        //dodavanje izvodjaca
+        public void Dodaj()
+        {
+            Dictionary<int, Izvodjac> sviIzvodjaci = UcitajIzvodjace();
+            if (sviIzvodjaci == null)
+            {
+                sviIzvodjaci = new Dictionary<int, Izvodjac>();
+            }
+            if (sviIzvodjaci.ContainsKey(Id))
+            {
+                throw new Exception("Izvodjac vec postoji");
+            }
+            sviIzvodjaci[Id] = this;
+            UpisiIzvodjace(sviIzvodjaci);
+        }
+        //izmeni izvodjaca
+        public void Izmeni(string _ime, int _prosecnaOcena, string _opis, MuzickaGrupa _muzickaGrupa,
+            List<Zanr> _sviZanrovi, List<Recenzija> _sveRecenzije, List<MuzickaNumera> _numere)
+        {
+            Ime = _ime;
+            ProsecnaOcena = _prosecnaOcena;
+            Opis = _opis;
+            Grupa = _muzickaGrupa;
+            SviZanrovi = _sviZanrovi;
+            SveRecenzije = _sveRecenzije;
+            Numere = _numere;
+
+            Dictionary<int, Izvodjac> sviIzvodjaci = UcitajIzvodjace();
+            if (!sviIzvodjaci.ContainsKey(Id))
+            {
+                throw new Exception("Ne postoji trazeni izvodjac");
+            }
+            sviIzvodjaci[Id] = this;
+            UpisiIzvodjace(sviIzvodjaci);
+        }
+        //obrisi izvodjaca
+        public void Obrisi()
+        {
+            Dictionary<int, Izvodjac> sviIzvodjaci = UcitajIzvodjace();
+            if (!sviIzvodjaci.ContainsKey(Id))
+            {
+                throw new Exception("Ne postoji trazeni izvodjac");
+            }
+            sviIzvodjaci.Remove(Id);
+            UpisiIzvodjace(sviIzvodjaci);
         }
     }
 }
