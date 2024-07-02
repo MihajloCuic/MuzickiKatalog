4033d1376befbbbfc837a45d2420f8a3e66ff654
milosm1707 <137085530+milosm1707@users.noreply.github.com>
2024-07-02 21:40:45 +0200

    UPDATE ElementiSistema
    Converter za elemente sistema prebacen u folder Helpers

--- MuzickiKatalog/obj/Debug/net8.0-windows/MuzickiKatalog.AssemblyInfo.cs
+++ MuzickiKatalog/obj/Debug/net8.0-windows/MuzickiKatalog.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MuzickiKatalog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fb0b7ee4d7f046c511d3ee235fe1e498fcd4bdb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f00ade98298fac709388b6d6f272b57a977be427")]
 [assembly: System.Reflection.AssemblyProductAttribute("MuzickiKatalog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MuzickiKatalog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
